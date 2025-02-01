a3306c63568c96aaaa13657bd7440d40364834f7
mk-gg <mk.gregy@gmail.com>
2025-02-01 19:41:33 +0800

    changed the type captured_message in models.py to text to accomodate large messages

--- backend/models.py
+++ backend/models.py
@@ -42,7 +42,7 @@ class Bans(db.Model):
     member_id = db.Column(db.String(20), db.ForeignKey('member.member_id'), nullable=False)
     server_id = db.Column(db.String(20), db.ForeignKey('server.server_id'), nullable=False)
     reason = db.Column(db.String(200), nullable=True)
-    captured_message = db.Column(db.String(500), nullable=True)
+    captured_message = db.Column(db.Text, nullable=True)
     created_at = db.Column(db.DateTime(timezone=True), nullable=False, default=lambda: datetime.now(timezone.utc))
 
     def to_json(self):
