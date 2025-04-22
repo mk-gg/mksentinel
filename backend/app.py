35c450a22c2733a8112575a2cc5f2b2b4d937c2e
mkgg <mk.gregy@gmail.com>
2025-04-22 14:38:46 +0800

    added github oauth provider in backend

--- backend/app.py
+++ backend/app.py
@@ -31,6 +31,7 @@ def create_app():
     app.config['SECRET_KEY'] = 'top secret!'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres.cpeyxarnnshkhhjfffsp:VOoLSxZeDkFRymUs@aws-0-us-west-1.pooler.supabase.com:6543/postgres'
     app.config['OAUTH2_PROVIDERS'] = {
+        # Google OAuth Provider
         'google': {
             'client_id': os.environ.get('GOOGLE_CLIENT_ID'),
             'client_secret': os.environ.get('GOOGLE_CLIENT_SECRET'),
@@ -42,6 +43,20 @@ def create_app():
             },
             'scopes': ['https://www.googleapis.com/auth/userinfo.email'],
         },
+
+        # Github OAuth Provider
+        # Documentation:  https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/authorizing-oauth-apps
+        'github': {
+        'client_id': os.environ.get('GITHUB_CLIENT_ID'),
+        'client_secret': os.environ.get('GITHUB_CLIENT_SECRET'),
+        'authorize_url': 'https://github.com/login/oauth/authorize',
+        'token_url': 'https://github.com/login/oauth/access_token',
+        'userinfo': {
+            'url': 'https://api.github.com/user/emails',
+            'email': lambda json: json[0]['email'],
+        },
+        'scopes': ['user:email'],
+    },
     }
     app.config['ADMIN_EMAILS'] = os.environ.get('ADMIN_EMAILS', '').split(',')
     app.config['API_KEY_SENTINEL'] = os.environ.get('API_KEY_SENTINEL')
--- backend/vercel_app.py
+++ backend/vercel_app.py
@@ -33,6 +33,7 @@ def create_app():
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'top secret!')
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql://postgres.cpeyxarnnshkhhjfffsp:VOoLSxZeDkFRymUs@aws-0-us-west-1.pooler.supabase.com:6543/postgres')
     app.config['OAUTH2_PROVIDERS'] = {
+        # Google OAuth Provider
         'google': {
             'client_id': os.environ.get('GOOGLE_CLIENT_ID'),
             'client_secret': os.environ.get('GOOGLE_CLIENT_SECRET'),
@@ -44,6 +45,20 @@ def create_app():
             },
             'scopes': ['https://www.googleapis.com/auth/userinfo.email'],
         },
+
+        # Github OAuth Provider
+        # Documentation:  https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/authorizing-oauth-apps
+        'github': {
+        'client_id': os.environ.get('GITHUB_CLIENT_ID'),
+        'client_secret': os.environ.get('GITHUB_CLIENT_SECRET'),
+        'authorize_url': 'https://github.com/login/oauth/authorize',
+        'token_url': 'https://github.com/login/oauth/access_token',
+        'userinfo': {
+            'url': 'https://api.github.com/user/emails',
+            'email': lambda json: json[0]['email'],
+        },
+        'scopes': ['user:email'],
+    },
     }
     app.config['ADMIN_EMAILS'] = os.environ.get('ADMIN_EMAILS', '').split(',')
     app.config['API_KEY_SENTINEL'] = os.environ.get('API_KEY_SENTINEL')
